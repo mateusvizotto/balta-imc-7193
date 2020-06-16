109c74c9c4ef5d548867d17d054cd0696bc52724
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-16 11:39:55 -0300

    imc-7193

---
+++ lib/ui/ios/pages/home.page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:imc/blocs/imc.bloc.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var bloc = new ImcBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("Cálculo do IMC"),
+        ),
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20),
+              //espaçamento de 20 pixeis em qualquer direção
+              child: CupertinoTextField(
+                placeholder: "Altura (cm)",
+                controller: bloc.heightCtrl,
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              //espaçamento de 20 pixeis em qualquer direção
+              child: CupertinoTextField(
+                placeholder: "Peso (kg)",
+                controller: bloc.weightCtrl,
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              //espaçamento de 20 pixeis em qualquer direção
+              child: Text(
+                bloc.result,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: CupertinoButton.filled(
+                child: Text(
+                  "Calcular",
+                ),
+                onPressed: () {
+                  setState(() {
+                    bloc.calculate();
+                  });
+                },
+              ),
+            ),
+          ],
+        ));
+  }
+}
